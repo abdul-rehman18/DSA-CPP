2221a46f6cfe5840b4889d27a00fe0290ee1cb39
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-07 06:28:21 +0500

    Implemented Source.cpp file

--- LinkedList/Source.cpp
+++ LinkedList/Source.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    
+    myLinkedList obj;
+    obj.insertAtTail(100);
+    obj.insertAtTail(23);
+    obj.insertAtTail(44);
+    obj.insertAtHead(92);
+    obj.display();
     return 0;
 }
