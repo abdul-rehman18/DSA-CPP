a18c65bc0e6fe733f53d43d2db94876a9f7c2bb4
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-07 06:25:50 +0500

    Implemented Delete From tail function

--- LinkedList/myLinkedList.h
+++ LinkedList/myLinkedList.h
@@ -60,3 +60,39 @@ void myLinkedList::display()
     }
     
 }
+
+int myLinkedList::deleteFromTail()
+{
+    if(head == nullptr && tail == nullptr)
+    {
+        cout << "LinkedList is empty...." << endl;
+        return NULL;
+    }
+    else if(head==tail)
+    {
+        int rv = head->data;
+        delete head;
+        //or
+        //delete tail;
+        head =  nullptr;
+        tail = nullptr;
+        return rv;
+    }
+    else
+    {
+        Node* temp = head;
+        while (1)
+        {
+            if(temp->next == tail)
+                break;
+            temp = temp->next;
+        }
+        int rv = tail->data;
+        delete tail;
+        tail = temp;
+        temp->next = nullptr;
+        //or
+        //tail->next = nullptr;
+        return rv;
+    }
+}
