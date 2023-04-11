2f38d99f30f214f2aa655686701f7ea5b5098699
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-11 06:30:02 +0500

    Stack and MyStack Class Defined and Declared

---
+++ Stack-Using-LinkedList/MyStack.h
@@ -0,0 +1,64 @@
+#include "Stack.h"
+
+
+class myStack :public Stack
+{
+private:
+    myLinkedList stk;
+public:
+	void push(int);
+	int pop();
+	int top();
+	myStack(int ss);
+	bool isEmpty();
+	void display();
+};
+
+
+bool myStack::isEmpty()
+{
+	if (stk.head == nullptr && stk.tail == nullptr)
+        return true;
+	return false;
+}
+
+
+void myStack::display()
+{
+	cout << "Maximum Size: " << Stack::maxSize << endl;
+	cout << "Current Size: " << Stack::currentSize << endl;
+
+	stk.display();
+}
+
+
+
+myStack::myStack(int ss):Stack(ss)
+{
+
+}
+
+
+void myStack::push(int value)
+{
+    stk.insertAtTail(value);
+}
+
+
+
+int myStack::pop()
+{
+	if (isEmpty())
+	{
+		cout << "Stack is Empty, returning ZERO" << endl;
+		return 0;
+	}
+    stk.deleteFromTail();
+}
+
+
+
+int myStack::top()
+{
+    
+}
--- Stack-Using-LinkedList/Stack.h
+++ Stack-Using-LinkedList/Stack.h
@@ -1,23 +1,22 @@
+#include"MyLinkedList.h"
+
 #include <iostream>
 using namespace std;
 
-template <class T>
 class Stack
 {
 protected:
-	T* arr;
+	int* arr;
 	int maxSize;
 	int currentSize;
 public:
-	virtual void push(T) = 0; 	//add Value
-	virtual T pop() = 0;		//remove Value
-	virtual T top() = 0;
-	virtual bool isFull() = 0;
+	virtual void push(int) = 0; 	//add Value
+	virtual int pop() = 0;		//remove Value
+	virtual int top() = 0;
 	virtual bool isEmpty() = 0;
 	Stack(int);
 };
 
-template <class T>
 Stack<T>::Stack(int s)
 {
 	arr = new T[s];
