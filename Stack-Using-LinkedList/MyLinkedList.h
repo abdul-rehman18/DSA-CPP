c105ce35d6c10e07734a90569711a64647aabc1d
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-11 06:46:23 +0500

    MyLinkedList done

--- Stack-Using-LinkedList/MyLinkedList.h
+++ Stack-Using-LinkedList/MyLinkedList.h
@@ -52,5 +52,73 @@ void myLinkedList::insertAtHead(int)
 
 void myLinkedList::display()
 {
-    
+    Node* temp = head;
+    while(1)
+    {
+        if (temp == nullptr)
+            break;
+        cout << temp->data <<endl;
+        temp = temp->next;
+    }
+}
+
+int myLinkedList::deleteFromTail()
+{
+    if (head == nullptr && tail == nullptr)
+    {
+        cout << "LinkedList is empty........" << endl;
+        return NULL;
+    }
+    else if (head == tail)
+    {
+        int rv = head->data;
+        delete head;
+        //or
+        //delete tail;
+        head =  nullptr;
+        tail = nullptr;
+        return rv;
+    }
+    else
+    {
+        Node* temp = head;
+        while (1)
+        {
+            if (temp->next == tail)
+                break;
+            temp = temp->next;
+        }
+        int rv = tail->data;
+        delete tail;
+        tail = temp;
+        temp->next = nullptr;
+        //or
+        //tail->next = nullptr;
+        return rv;
+    }
+}
+
+int myLinkedList::deleteFromHead()
+{
+    if (head == nullptr && tail == nullptr)
+    {
+        cout << "LinkedList is empty...." << endl;
+        return NULL;
+    }
+    else if (head == tail)
+    {
+        int rv = head->data;
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+        return rv;
+    }
+    else
+    {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        temp = nullptr;
+        return rv;
+    }
 }
