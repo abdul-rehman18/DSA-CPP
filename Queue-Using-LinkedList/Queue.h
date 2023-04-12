e247f407e2e7646eb54902eb5f854c2d1b663aac
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-12 07:11:08 +0500

    Queue.h created

---
+++ Queue-Using-LinkedList/Queue.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+protected:
+	int* arr;
+	int maxSize;
+	int currentSize;
+public:
+	virtual void enqueue(int) = 0;
+	virtual int dequeue() = 0;
+	virtual bool isFull() = 0;
+	virtual bool isEmpty() = 0;
+	Queue(int);
+};
+
+Queue::Queue(int s)
+{
+	arr = new T[s];
+	maxSize = s;
+	currentSize = 0;
+}
