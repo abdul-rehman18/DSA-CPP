0bc1a3ca6247d3170ad640baa15d89234ec7575d
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-13 06:56:17 +0500

    Stack Using LinkedList done

--- Stack-Using-LinkedList/Source.cpp
+++ Stack-Using-LinkedList/Source.cpp
@@ -1,6 +1,15 @@
-#include"MyStack.h"
-
+#include "MyStack.h"
 int main()
 {
-    return 0;
+	myStack two(4);
+	cout << two.pop() << endl;
+	two.push(235);
+	two.push(10);
+	two.push(-3);
+	two.push(17);
+	two.push(88);
+	two.display();
+	cout << endl;
+
+	return 0;
 }
