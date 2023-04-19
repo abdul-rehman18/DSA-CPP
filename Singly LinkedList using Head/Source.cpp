98049d07cc64748a9e41fe408fc049f31778ae9b
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-19 05:34:46 +0500

    Made some changes

---
+++ Singly LinkedList using Head/Source.cpp
@@ -0,0 +1,26 @@
+#include "myLinkedList.h"
+
+int main()
+{
+	myLinkedList obj;
+
+	obj.insertAtHead(50);
+	obj.insertAtHead(2);
+	obj.insertAtHead(0);
+	obj.insertAtTail(787);
+	obj.insertAtTail(-2);
+
+	cout << "Delete From Tail: " << obj.deleteFromTail() << endl;
+
+	obj.insertSorted(1462);
+
+	cout << obj.deleteValue(1462) << endl;
+	
+	cout << " Display:" << endl;
+
+	cout << obj.deleteValue(1462) << endl << " Display:" << endl;
+
+
+	obj.display();
+	return 0;
+}
