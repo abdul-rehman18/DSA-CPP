cfb915c99df4e5d603f8a280acc550156776a366
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-04-27 23:04:03 +0500

    LinkedList.h implemented

---
+++ Palindrome using LinkedList/LinkedList.h
@@ -0,0 +1,31 @@
+#include"Node.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<class T>
+class Linkedlist
+{
+protected:
+	Node<T>* head;
+	Node<T>* tail;
+public:
+	Linkedlist();
+	virtual bool isEmpty() = 0;
+	virtual void insertAtTail(T) = 0;
+	virtual void insertAtHead(T) = 0;
+	virtual void insertSorted(T) = 0;
+	virtual void display() = 0;
+	virtual bool deleteFromTail() = 0;
+	virtual bool deleteFromHead() = 0;
+	virtual bool deleteValue(T) = 0;
+	virtual bool search(T) = 0;
+	virtual void palindrome() = 0;
+};
+
+template<class T>
+Linkedlist<T>::Linkedlist()
+{
+	head = nullptr;
+	tail = nullptr;
+}
