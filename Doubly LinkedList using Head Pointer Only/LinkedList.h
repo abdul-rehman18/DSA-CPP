4d3f42aecb7340c515733c3f252d8e61d32247d2
Abdul Rehman Nadeem <abdulrehmanadeem14@gmail.com>
2023-06-04 16:43:45 +0500

    LinkedList.h implemented

---
+++ Doubly LinkedList using Head Pointer Only/LinkedList.h
@@ -0,0 +1,22 @@
+#include"Node.h"
+#include<iostream>
+class LinkedList{
+protected:
+  Node* head;
+  
+public:
+  LinkedList();
+  virtual void insertAtTail(int)=0;
+  virtual void insertAtHead(int)=0;
+  virtual int deleteFromHead()=0;
+  virtual int deleteFromTail()=0;
+  virtual bool search(int)=0;
+  virtual void display()=0;
+  ~LinkedList();
+};
+LinkedList::LinkedList(){
+        head=nullptr;
+}
+LinkedList::~LinkedList(){
+    delete head;
+}
